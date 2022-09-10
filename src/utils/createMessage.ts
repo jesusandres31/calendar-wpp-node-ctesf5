d36ce322d86e6734ea54388f7af6487358e57c41
Corrientes Futbol 5 <corrientesfut5@gmail.com>
2022-09-09 22:32:52 -0300

    DeployMaqCtesF5

--- src/utils/createMessage.ts
+++ src/utils/createMessage.ts
@@ -1,3 +1,3 @@
 export const createMessage = (playerName: string, startTime: string) => {
-  return `Hola ${playerName}, te recordamos que hoy tenes turno a las ${startTime}hs. Te esperamos!`;
+  return `Hola ${playerName}, hoy tienen turno a las ${startTime}hs. Los esperamos! ⚽🏃`;
 };
