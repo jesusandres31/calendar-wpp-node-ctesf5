07daffcb2158a61a64a83ea5619f3ff3d72fe440
jesus.zini@devlights.com <jesusandreszini14@gmail.com>
2022-09-14 15:46:31 -0300

    formatName updated

--- src/utils/formatName.ts
+++ src/utils/formatName.ts
@@ -1,5 +1,6 @@
 export const formatName = (fullName: string) => {
-  let firstName = '';
-  firstName = fullName.split(' ')[0];
-  return firstName;
+  let str = '';
+  str = fullName.trim().toLowerCase().split(' ')[0];
+  str = str.charAt(0).toUpperCase() + str.slice(1);
+  return str;
 };
